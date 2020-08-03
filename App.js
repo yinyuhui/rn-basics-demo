0fbe95efde77b1f7c4f521fd5ea586feb6750eb9
尹玉慧 <veinyin@gmail.com>
2020-08-03 20:24:02 +0800

    使用矢量图标

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Text, View, Button } from 'react-native'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 const HelloWorldApp = (props) => {
     return (
@@ -9,12 +10,15 @@ const HelloWorldApp = (props) => {
                 justifyContent: 'center',
                 alignItems: 'center',
             }}>
+            <FontAwesome name="th-list" size={20} style={{ color: 'red' }} />
+
             <Button
                 title="flatList demo"
                 onPress={() => {
                     props.navigation.navigate('FlatListDemo')
                 }}
             />
+            <FontAwesome name="list-alt" size={24} style={{ color: 'red' }} />
             <Button
                 title="sectionList demo"
                 onPress={() => {
